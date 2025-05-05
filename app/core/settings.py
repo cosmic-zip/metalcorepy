a5dfb218510f9f4dc77c2e9d9c6139361e60f359
Cassandra Lovelace <36008397+cosmic-zip@users.noreply.github.com>
2025-05-05 15:12:40 -0300

    Dev/foundation (#1)

--- app/core/settings.py
+++ app/core/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,15 +21,35 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x3^9t@ww25yc14^-!c5f3bcfp0pt0-g-!icl9p480$pjoou04+'
+BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-insecure-secret-key')
+DEBUG = os.environ.get('DEBUG', 'False').lower() in ['true', '1', 'yes']
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '').split()
 
-ALLOWED_HOSTS = []
-
+DATABASES = {
+    'default': {
+        'ENGINE': os.environ.get('DB_ENGINE', ''),
+        'NAME': os.environ.get('POSTGRES_DB', ''),
+        'USER': os.environ.get('POSTGRES_USER', ''),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', ''),
+        'HOST': os.environ.get('POSTGRES_HOST', ''),
+        'PORT': os.environ.get('POSTGRES_PORT', ''),
+    }
+}
 
 # Application definition
+
+CUSTOM_APPS = [
+    'domains.domain_name'
+]
+
+CORE_APPS = [
+    'infrastructure',
+    'shared',
+    'domains',
+    'interfaces',
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -37,7 +58,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+] + CUSTOM_APPS + CORE_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -73,12 +94,6 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
@@ -103,19 +118,22 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_TZ = True
+LANGUAGE_CODE = os.environ.get('LANGUAGE_CODE', 'en-us')
+TIME_ZONE = os.environ.get('TIME_ZONE', 'UTC')
+USE_I18N = os.environ.get('USE_I18N', 'True').lower() in ['true', '1', 'yes']
+USE_TZ = os.environ.get('USE_TZ', 'True').lower() in ['true', '1', 'yes']
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',  # e.g., project-level /static/ directory
+]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
---
+++ app/domains/domain_name/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Domain_nameConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'domains.domain_name'
