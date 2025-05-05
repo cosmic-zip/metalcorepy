bb4fe2a90eb0ee4e7afb3a8b950b085f7b528f15
cosmic-zip <36008397+cosmic-zip@users.noreply.github.com>
2025-05-05 14:10:28 -0300

    update

--- app/core/settings.py
+++ app/core/settings.py
@@ -34,11 +34,11 @@ if SQL_ENGINE:
     DATABASES = {
         'default': {
             'ENGINE': SQL_ENGINE,
-            'NAME': os.environ.get('SQL_DATABASE', ''),
-            'USER': os.environ.get('SQL_USER', ''),
-            'PASSWORD': os.environ.get('SQL_PASSWORD', ''),
-            'HOST': os.environ.get('SQL_HOST', ''),
-            'PORT': os.environ.get('SQL_PORT', ''),
+            'NAME': os.environ.get('POSTGRES_DB', ''),
+            'USER': os.environ.get('POSTGRES_USER', ''),
+            'PASSWORD': os.environ.get('POSTGRES_PASSWORD', ''),
+            'HOST': os.environ.get('POSTGRES_HOST', ''),
+            'PORT': os.environ.get('POSTGRES_PORT', ''),
         }
     }
 else:
