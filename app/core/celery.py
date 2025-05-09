dd5bd8d3e628a7c5250d2035cd2ceb88672466f1
Ysrial <wyvigk5@gmail.com>
2025-05-09 10:38:36 -0300

    ➕ Add celery
    add celery

---
+++ app/core/celery.py
@@ -0,0 +1,18 @@
+import os
+from celery import Celery
+
+# Define o módulo de configurações padrão do Django para o Celery
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+# Cria uma instância do Celery
+app = Celery('core')
+
+# Configuração usando as configurações do Django
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Auto-descoberta de tarefas nos aplicativos instalados
+app.autodiscover_tasks()
+
+@app.task(bind=True, ignore_result=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}') 
--- app/core/settings.py
+++ app/core/settings.py
@@ -139,3 +139,14 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Celery Configuration
+CELERY_BROKER_URL = env('CELERY_BROKER_URL', default='redis://localhost:6379/0')
+CELERY_RESULT_BACKEND = env('CELERY_RESULT_BACKEND', default='redis://localhost:6379/0')
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = TIME_ZONE
+
+# Importar as configurações do Beat
+from .celery_beat import CELERY_BEAT_SCHEDULE
